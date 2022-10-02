ef1d94e838bbed051cc7c935aafdc1967ac85c27
unknown <eduardoasm@gmail.com>
2022-10-02 18:43:03 -0400

    create database mongodb

---
+++ models/models.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+import { Schema, model, models } from 'mongoose'
+
+interface UserModel {
+    username: string;
+    email: string;
+    password: string;
+}
+
+const userSchema = new Schema<UserModel>(
+    {
+        username:{
+            type: String,
+            require: true,
+            unique: true
+        },
+        email:{
+            type: String,
+            require: true,
+            unique: true
+        },
+        password:{
+            type: String,
+            require: true,
+        },
+
+    }
+)
+
+const User = models.User || model<UserModel>("User", userSchema)
+
+export default User
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import connectMongo from "../.././utils/connectDb"
+import User from "../.././models/models"
 
 type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler( req: NextApiRequest,res: NextApiResponse<Data>) {
+  console.log("CONNECTING MONGO")
+  await connectMongo();
+  console.log("MONGO CONNECT")
+
+  console.log("creatin document")
+  // interface UserObj {
+  //   username: string;
+  //   password: string;
+  //   email: string;
+  // }
+
+  const userTest = await User.create(req.body)
+  res.send(userTest)
+  console.log("created document")
+
 }
+
+
