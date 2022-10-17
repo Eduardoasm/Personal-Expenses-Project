50440eef40b10c75007763a00c791b2f0a5f332f
unknown <eduardoasm19@gmail.com>
2022-10-17 19:19:08 -0400

    css register and login

--- pages/register.tsx
+++ pages/register.tsx
@@ -40,46 +40,74 @@ export default function Register() {
     }
   };
 
+  const buttonHome = async (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    return router.push("/")
+  }
+
   return (
     <div className={styles.main}>
         <div className={styles.register}>
+            <div className={styles.containerRegister}>
           <form 
           className={styles.form}
           onSubmit={(e) => register(e)}>
-            <label>username</label>
+            <label className={styles.labelsStyles}>
+            Username
+            </label>
             <input
+            placeholder="Username"
             className={styles.input}
               name="username"
               type="username"
               value={inputChange.username}
               onChange={handleChange}
             />
-            <label>email</label>
+            <label className={styles.labelsStyles}>
+            Email
+            </label>
             <input
+            placeholder="Email"
             className={styles.input}
               name="email"
               type="email"
               value={inputChange.email}
               onChange={handleChange}
             />
-            <label>password</label>
+            <label className={styles.labelsStyles}>
+            Password
+            </label>
             <input
+            placeholder="Password"
             className={styles.input}
               name="password"
               type="password"
               value={inputChange.password}
               onChange={handleChange}
             />
-            <label>repeat password</label>
+            <label className={styles.labelsStyles}>
+            Repeat Password
+            </label>
             <input
+            placeholder="Password"
               className={styles.input}
               name="repeatPassword"
               type="password"
               value={inputChange.repeatPassword}
               onChange={handleChange}
             />
-            <button>register</button>
+            <button className={styles.button1}>
+            register
+            </button>
           </form>
+          <label className={styles.labelsStyles1}>
+            Have account? log in Here
+          </label>
+          <button className={styles.button1}
+          onClick={(e) => buttonHome(e)}>
+            Log In
+          </button>
+            </div>
         </div>
     </div>
   );
