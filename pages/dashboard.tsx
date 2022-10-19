272983b17967648e5876c9a576f137c84f237779
unknown <eduardoasm19@gmail.com>
2022-10-19 19:09:35 -0400

    css dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,96 +1,103 @@
-import React from 'react'
-import axios from 'axios'
-import { useRouter } from 'next/router'
-import { useState } from 'react'
-import { FormEvent, ChangeEventHandler } from 'react'
-import styles from "../styles/dashboard.module.css"
-import { BsPiggyBank, BsBank2 } from 'react-icons/bs'
-import {  GiPayMoney } from 'react-icons/gi'
+import React from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { FormEvent, ChangeEventHandler } from "react";
+import styles from "../styles/dashboard.module.css";
+import { BsPiggyBank, BsBank2 } from "react-icons/bs";
+import { GiPayMoney } from "react-icons/gi";
+import { BiLogOut } from 'react-icons/bi'
 
+export default function Dashboard() {
+  interface numberUser {
+    add: number;
+  }
 
-export default function Dashboard(){
+  // const [add, setAdd] = useState<number>(0)
+  const [add, setAdd] = useState<numberUser>({
+    add: 0,
+  });
+  const router = useRouter();
 
-    interface numberUser{
-        add: number
+  // const buttonClick = async () => {
+  //     const response = await axios.get("/api/user")
+  //     console.log(response)
+  // }
+
+  const logoutUser = async () => {
+    try {
+      const response = await axios.post("/api/auth/logout");
+      console.log(response);
+      router.push("/");
+    } catch (error) {
+      router.push("/");
+      console.log(error);
     }
-    
-    // const [add, setAdd] = useState<number>(0)
-    const [add, setAdd] = useState<numberUser>({
-        add: 0
-    })
-    const router = useRouter()
-    
-    // const buttonClick = async () => {
-    //     const response = await axios.get("/api/user")
-    //     console.log(response)
-    // }
-    
-    const logoutUser = async () => {
-        try {
-            const response = await axios.post("/api/auth/logout")
-            console.log(response)
-            router.push("/")
-        } catch (error) {
-            router.push("/")
-            console.log(error)
-        }
-    }
+  };
 
-    const handleChange: ChangeEventHandler<HTMLInputElement>  = (e) => {
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     e.preventDefault();
     // setAdd(parseInt(e.currentTarget.value))
     setAdd({
-        ...add,
-        [e.currentTarget.name]: e.currentTarget.value
-    })
+      ...add,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
     // console.log(add)
+  };
+
+  const addMoney = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      console.log("soy add", add);
+      const response = await axios.post("/api/walletUser/sumBalance", add);
+      console.log(response);
+    } catch (error) {
+      console.log("toy mandando error", error);
     }
+  };
 
-    const addMoney = async (e:FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        try {
-            console.log("soy add", add)
-            const response = await axios.post("/api/walletUser/sumBalance", add)
-            console.log(response)
-        } catch (error) {
-            console.log("toy mandando error", error)
-        }
-    }
-
-    return(
-        <div className={styles.main}>
-            <div className={styles.sidebar}>
-            <ul className={styles.ul}>
-                <li>
-
-            <BsPiggyBank/>
-            <p>
-            Deposit  
+  return (
+    <div className={styles.main}>
+      <div className={styles.sidebar}>
+        <ul className={styles.ul}>
+          <li className={styles.active}>
+            <p className={styles.icon}>
+              <BsPiggyBank />
             </p>
-    
-                </li>
-                <li>
-            <GiPayMoney/>
-                Withdraw
-                </li>
-            </ul>
-            </div>
-            <div>
-            <form onSubmit={(e) => addMoney(e)}>
-            <input
+            <p>
+              Deposit
+            </p>
+          </li>
+          <li>
+            <p className={styles.icon}>
+              <GiPayMoney />
+            </p>
+            <p>
+              Withdraw
+            </p>
+          </li>
+          <li onClick={() => logoutUser()}>
+            <p className={styles.icon}> 
+        <BiLogOut>
+        </BiLogOut>
+            </p>
+        <p>
+        Logout
+        </p>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <form onSubmit={(e) => addMoney(e)}>
+          <input
             type="number"
-            name='add'
+            name="add"
             value={add.add}
             onChange={handleChange}
-            />
-            <button>
-                agregar
-            </button>
-            </form>
-            <button onClick={() => logoutUser()}>
-                logout
-            </button>
-            </div>
-        </div>
-    )
+          />
+          <button>agregar</button>
+        </form>
+      </div>
+    </div>
+  );
 }
